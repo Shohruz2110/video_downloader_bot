97bd38ad52035178ceeccb0cd0b442730a6a9265
Shohruz2110 <ismatilaevshohruz@gmail.com>
2025-05-20 10:07:45 +0500

    start yangi  cod qo`shild

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
 from aiogram.utils import executor
 import os
 
@@ -8,8 +9,8 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
-async def start_handler(message: types.Message):
-    await message.answer(
-        "Salom! Men video yuklab beruvchi Telegram botman.\n"
-        "Instagram, TikTok yoki YouTube havolasini yuboring — men sizga videoni yuboraman."
-    )
+async def send_welcome(message: types.Message):
+    await message.reply("Salom! Men video yuklab beruvchi botman. Instagram, TikTok va YouTube’dan video link yuboring.")
+
+if name == 'main':
+    executor.start_polling(dp, skip_updates=True)
